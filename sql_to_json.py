b05cc1f9983de2b82b5e710d8a4055b9231bc273
Zach H <Zoosorbs@gmail.com>
2016-07-15 16:50:58 -0400

    split cards to sets
    Former-commit-id: 39067cf6182860a7399b1afd751ee958460c1986

--- sql_to_json.py
+++ sql_to_json.py
@@ -16,25 +16,32 @@ def db_to_json(database_connection):
     database_connection.row_factory = sqlite3.Row # Enable keys for the rows
     cursor = database_connection.cursor()
 
-    cursor.execute(""" SELECT * from cards ORDER BY setCode""")
+    cursor.execute("SELECT DISTINCT setCode from cards")
 
-# TODO: Figure out how to put them into LEA {cards}, LEB {cards}, ..., SOI {cards}
+    mainDict = []
     returnData = ""
     rows = cursor.fetchall()
-    for row in rows:
-        row = remove_empty_keys(dict_from_row(row))
-        dump = json.dumps(row, sort_keys=True)
-        returnData += dump
-    
+    for setCode in rows:
+        setCode = remove_empty_keys(dict_from_row(setCode))
+        cursor.execute("SELECT * FROM cards WHERE setCode = '%s'" % setCode["setCode"])
+        card_rows = cursor.fetchall()
+
+        for row in card_rows:
+            row = remove_empty_keys(dict_from_row(row))
+            dump = json.dumps(row, sort_keys=True)
+            returnData += dump
+        
+        mainDict.append([setCode, returnData])
+        returnData = ""
     database_connection.close()
-    return returnData
+    return str(mainDict)
     
 def main():
     d = os.path.join(os.path.expanduser(input("Location of database: ")), "Magic DB.db")
     d = sqlite3.connect(d)
-    
+
     xml = os.path.join(os.path.expanduser(input("Location of save file: ")), "Output.json")
-        
+
     json_code = db_to_json(d)
     
     writeFile = open(xml, 'w')
