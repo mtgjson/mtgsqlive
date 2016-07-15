1bb285e2fb94e74936203362385fc68a3e43a68b
Zach H <Zoosorbs@gmail.com>
2016-07-15 17:49:28 -0400

    wip
    Former-commit-id: 81364719376ace198a2c0a9102f7e0eecc5e8f16 [formerly 9099f593462f994a5088d403472fa61b74c8d3b6]
    Former-commit-id: 428bb1207d9512d19ac2c285be64baa60578f008

--- sql_to_json.py
+++ sql_to_json.py
@@ -16,33 +16,35 @@ def db_to_json(database_connection):
     database_connection.row_factory = sqlite3.Row # Enable keys for the rows
     cursor = database_connection.cursor()
 
-    cursor.execute("SELECT DISTINCT setCode from cards")
+    cursor.execute("SELECT DISTINCT setCode from cards LIMIT 3,5")
 
     mainDict = []
-    returnData = ""
+    returnData = []
     rows = cursor.fetchall()
     for setCode in rows:
         setCode = remove_empty_keys(dict_from_row(setCode))
-        cursor.execute("SELECT * FROM cards WHERE setCode = '%s'" % setCode["setCode"])
+        cursor.execute("SELECT * FROM cards WHERE setCode = '%s' LIMIT 5" % setCode["setCode"])
         card_rows = cursor.fetchall()
 
         for row in card_rows:
             row = remove_empty_keys(dict_from_row(row))
-            dump = json.dumps(row, sort_keys=True)
-            returnData += dump
-        
+
+            returnData.append(row)
+
         mainDict.append([setCode, returnData])
-        returnData = ""
+        returnData = []
     database_connection.close()
-    return str(mainDict)
+    return mainDict
     
 def main():
-    d = os.path.join(os.path.expanduser(input("Location of database: ")), "Magic DB.db")
+    #d = os.path.join(os.path.expanduser(input("Location of database: ")), "Magic DB.db")
+    d = os.path.join(os.path.expanduser("~/Desktop"), "Magic DB.db")
     d = sqlite3.connect(d)
 
-    xml = os.path.join(os.path.expanduser(input("Location of save file: ")), "Output.json")
+    #xml = os.path.join(os.path.expanduser(input("Location of save file: ")), "Output.json")
+    xml = os.path.join(os.path.expanduser("~/Desktop"), "Output.json")
 
-    json_code = db_to_json(d)
+    json_code = json.dumps(db_to_json(d), indent=4, sort_keys=True)
     
     writeFile = open(xml, 'w')
     writeFile.write(json_code)
