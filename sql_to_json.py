89895ed75f218fa38cb774a59a5b1c7f8db4457f
Zach H <Zoosorbs@gmail.com>
2016-07-15 23:56:19 -0400

    wip 2
    Former-commit-id: 8a0e745641bd75a1cc3c1f88a06a23482afe0762

--- sql_to_json.py
+++ sql_to_json.py
@@ -2,6 +2,12 @@
 import json
 import sqlite3
 import os
+import fileinput
+
+def get_next(some_iterable, window=1):
+    items, nexts = tee(some_iterable, 2)
+    nexts = islice(nexts, window, None)
+    return izip_longest(items, nexts)
 
 def dict_from_row(row):
     return dict(zip(row.keys(), row))
@@ -16,19 +22,18 @@ def db_to_json(database_connection):
     database_connection.row_factory = sqlite3.Row # Enable keys for the rows
     cursor = database_connection.cursor()
 
-    cursor.execute("SELECT DISTINCT setCode from cards LIMIT 3,5")
+    cursor.execute("SELECT DISTINCT setCode from cards LIMIT 10")
 
     mainDict = []
     returnData = []
     rows = cursor.fetchall()
     for setCode in rows:
         setCode = remove_empty_keys(dict_from_row(setCode))
-        cursor.execute("SELECT * FROM cards WHERE setCode = '%s' LIMIT 5" % setCode["setCode"])
+        cursor.execute("SELECT * FROM cards WHERE setCode = '%s'" % setCode["setCode"])
         card_rows = cursor.fetchall()
 
         for row in card_rows:
             row = remove_empty_keys(dict_from_row(row))
-
             returnData.append(row)
 
         mainDict.append([setCode, returnData])
@@ -42,13 +47,52 @@ def main():
     d = sqlite3.connect(d)
 
     #xml = os.path.join(os.path.expanduser(input("Location of save file: ")), "Output.json")
-    xml = os.path.join(os.path.expanduser("~/Desktop"), "Output.json")
+    xml = os.path.join(os.path.expanduser("~/Desktop"), "Output.tmp.json")
+    xml2 = os.path.join(os.path.expanduser("~/Desktop"), "Output.json")
 
-    json_code = json.dumps(db_to_json(d), indent=4, sort_keys=True)
+    json_code = json.dumps(db_to_json(d), sort_keys=True, indent=2)
     
     writeFile = open(xml, 'w')
     writeFile.write(json_code)
     writeFile.close()
+    
+    with open(xml) as f:
+        with open(xml2, 'w') as f2:
+            for line in f.readlines():
+                if replace_and_write_these_keys(f2, line, "colorIdentity"): continue
+                if replace_and_write_these_keys(f2, line, "colors"): continue
+                if replace_and_write_these_keys(f2, line, "printings"): continue
+                if replace_and_write_these_keys(f2, line, "subtypes"): continue
+                if replace_and_write_these_keys(f2, line, "legalities"): continue
+                if replace_and_write_these_keys(f2, line, "types"): continue
+                #if replace_and_write_these_keys(f2, line, "rulings"): continue
+                #if replace_and_write_these_keys(f2, line, "foreignNames"): continue
+                
+                if '"' + "variations" + '":' in line:
+                    f2.write(",")
+                    
+                f2.write(line)
+        f2.close()
+    os.remove(xml)                     
+     
+def replace_and_write_these_keys(file_opened, line, key_val):
+    retVal = str_to_json(line, key_val)
+    if retVal:
+        file_opened.write(retVal)
+        return True
+
+def str_to_json(line, key_val):
+    if '"' + key_val + '":' in line:
+        line_index = line.index('"[')
+        line = line[:line_index] + line[line_index:].replace('\\"', '"')[1:]
+            
+        while line.strip()[-1:] != "]":
+            line = line[:-1]
+            
+        if key_val != "types":
+            line += ","
+        line += "\n"
+        return line      
 
 if __name__ == '__main__':
     main()
