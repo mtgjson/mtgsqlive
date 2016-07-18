31e9698c693c2a96e6441c3b60c0ad6091e52d8b
Zach H <Zoosorbs@gmail.com>
2016-07-18 01:11:33 -0400

    fenhl fixes
    Former-commit-id: 54c713e7d648b1590360ff6f30c40d93c97a56b0

--- testing_mac.sh
+++ testing_mac.sh
@@ -1,1 +1,8 @@
-./json_to_sql.py 1 ~/Desktop/Real.db ~/Desktop/AllSets-x.json; ./sql_to_json.py ~/Desktop/Real.db ~/Desktop/MyOutput.json; ./json_to_sql.py 1 ~/Desktop/Mine.db ~/Desktop/MyOutput.json; ./sql_to_json.py ~/Desktop/Mine.db ~/Desktop/MySecondOutput.json; diff ~/Desktop/MyOutput.json ~/Desktop/MySecondOutput.json > ~/Desktop/Diff.txt; md5sum ~/Desktop/MyOutput.json ~/Desktop/MySecondOutput.json
+#!/usr/bin/env bash
+
+./json_to_sql.py 1 ~/Desktop/Real.db ~/Desktop/AllSets-x.json;
+./sql_to_json.py ~/Desktop/Real.db ~/Desktop/MyOutput.json;
+./json_to_sql.py 1 ~/Desktop/Mine.db ~/Desktop/MyOutput.json;
+./sql_to_json.py ~/Desktop/Mine.db ~/Desktop/MySecondOutput.json;
+diff ~/Desktop/MyOutput.json ~/Desktop/MySecondOutput.json > ~/Desktop/Diff.txt;
+md5sum ~/Desktop/MyOutput.json ~/Desktop/MySecondOutput.json
