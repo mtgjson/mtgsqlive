73fe5da15a02f37148aa286ade94b90514285ce0
Zach H <Zoosorbs@gmail.com>
2016-07-19 00:49:33 -0400

    allcards and allsets

--- sql_to_json.py
+++ sql_to_json.py
@@ -19,10 +19,31 @@ def remove_set_info(dictionary):
     dictionary.pop("setReleaseDate", None)
     return dictionary
 
+def remove_unneeded(dictionary):
+    dictionary.pop("artist", None)
+    dictionary.pop("border", None)
+    dictionary.pop("flavor", None)
+    dictionary.pop("foreignNames", None)
+    dictionary.pop("id", None)
+    dictionary.pop("multiverseid", None)
+    dictionary.pop("number", None)
+    dictionary.pop("originalText", None)
+    dictionary.pop("originalType", None)
+    dictionary.pop("rarity", None)
+    dictionary.pop("releaseDate", None)
+    dictionary.pop("reserved", None)
+    dictionary.pop("timeshifted", None)
+    dictionary.pop("variations", None)
+    dictionary.pop("watermark", None)
+    dictionary.pop("imageName", None)
+    dictionary.pop("mciNumber", None)
+    dictionary = remove_set_info(dictionary)
+    return dictionary
+
 def set_dictionary(row):
     return dict(zip(row.keys(), row))
 
-def db_to_json(database_connection):
+def db_to_json_allsets(database_connection):
     database_connection.row_factory = sqlite3.Row # Enable keys for the rows
     cursor = database_connection.cursor()
     cursor.execute("SELECT DISTINCT setCode from cards")
@@ -32,38 +53,68 @@ def db_to_json(database_connection):
     rows = cursor.fetchall()
     for setCode in rows:
         setCode = set_dictionary(setCode)
-        cursor.execute("SELECT * FROM cards WHERE setCode = '%s'" % setCode["setCode"])
+        cursor.execute("SELECT * FROM cards WHERE setCode = ?" % [setCode["setCode"]])
         card_rows = cursor.fetchall()
 
         setName = None
         setReleaseDate = None
         for row in card_rows:
-            row = dict_from_row(row)
+            row = dict_from_row(row) # Turn SQL.Row -> Dictionary
 
+            # Set temporary variables used for JSON Sorting data for AllSets
             if not setName or not setReleaseDate:
                 setName = row["setName"]
                 setReleaseDate = row["setReleaseDate"]
 
+            # Remove temporary variables from the dictionary, as they're unneeded
             row = remove_set_info(row)
             returnData.append(row)
 
+        # Inset into dictionary the JSON data
         mainDict[setCode["setCode"]] = dict(zip(["cards", "name", "releaseDate"], [returnData, setName, setReleaseDate]))
+
+        # Reset variables
         setName = None
         setReleaseDate = None
         returnData = []
+
     database_connection.close()
 
     return mainDict
+
+def db_to_json_allcards(database_connection):
+    database_connection.row_factory = sqlite3.Row # Enable keys for the row
+    cursor = database_connection.cursor()
     
+    mainDict = {}
+    cursor.execute("SELECT DISTINCT name from cards ORDER BY name, setReleaseDate ASC")
+    rows = cursor.fetchall()
+
+    # This loop will take a while (~5 minutes) to complete. Be patient
+    for this_card in rows:
+        cursor.execute("SELECT * FROM cards WHERE name = ?", [this_card["name"]])
+        card_rows = cursor.fetchall()
+        for row in card_rows:
+            row = dict_from_row(row)
+            row = remove_unneeded(row)
+            mainDict[json.loads(this_card["name"])] = row
+    return mainDict
+
 def main():
-    if len(sys.argv) != 3:
-        print("Must provide 2 arguements: database_location, json_output_location")
+    if len(sys.argv) != 4:
+        print("Must provide 3 arguements: database_location, json_output_location, sets_or_cards")
         os._exit(1)
 
     db_path = sqlite3.connect(os.path.expanduser(sys.argv[1])) # File location for database
     file_path = os.path.expanduser(sys.argv[2]) # File location for output
     
-    dictionary = db_to_json(db_path)
+    if (sys.argv[3] == "sets"):
+        dictionary = db_to_json_allsets(db_path)
+    elif (sys.argv[3] == "cards"):
+        dictionary = db_to_json_allcards(db_path)
+    else:
+        print('Final arguement must be "sets" or "cards"')
+        os._exit(1)
 
     with open(file_path, 'w') as json_f:
         json.dump(dictionary, json_f, sort_keys=True, indent=4)
--- testing_mac.sh
+++ testing_mac.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ./json_to_sql.py 1 ~/Desktop/Real.db ~/Desktop/AllSets-x.json;
-./sql_to_json.py ~/Desktop/Real.db ~/Desktop/MyOutput.json;
+./sql_to_json.py ~/Desktop/Real.db ~/Desktop/MyOutput.json sets;
 ./json_to_sql.py 1 ~/Desktop/Mine.db ~/Desktop/MyOutput.json;
-./sql_to_json.py ~/Desktop/Mine.db ~/Desktop/MySecondOutput.json;
+./sql_to_json.py ~/Desktop/Mine.db ~/Desktop/MySecondOutput.json sets;
 diff ~/Desktop/MyOutput.json ~/Desktop/MySecondOutput.json > ~/Desktop/Diff.txt;
 md5sum ~/Desktop/MyOutput.json ~/Desktop/MySecondOutput.json
