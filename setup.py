ad2475438d45bf672115856e04a99f8159ea8f4b
Zach H <zahalpern+github@gmail.com>
2020-04-22 17:32:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 """Installation setup for MTGSQLite."""
 
+import pathlib
 import setuptools
 
 # Necessary for TOX
 setuptools.setup(
     name="MTGSQLite",
-    version=0.1,
+    version=0.2,
     author="Zach Halpern",
-    author_email="Zahalpern+github@gmail.com",
+    author_email="zach@mtgjson.com",
     url="https://github.com/mtgjson/mtgsqlive/",
-    description="Convert MTGJSONv4 files to SQLite format for Magic: The Gathering",
-    long_description=open("README.md", "r").read(),
+    description="Convert MTGJSONv4 and MTGJSONv5 files to SQLite format for Magic: The Gathering",
+    long_description=pathlib.Path("README.md").open().read(),
     long_description_content_type="text/markdown",
-    license="GPL-3.0",
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT",
     ],
     keywords="Magic: The Gathering, MTG, JSON, Card Games, Collectible, Trading Cards",
     packages=setuptools.find_packages(),
-    install_requires=["requests","pandas"],
+    install_requires=pathlib.Path("requirements.txt").open().readlines(),
 )
